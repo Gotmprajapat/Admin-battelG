4951bd70a2d96931ae9b547654e01a4b6c34be13
Gautam <gotmprajapat00@gmail.com>
2026-07-04 22:14:37 +0530

    Update and rename admin-login.js to login.js

--- js/login.js
+++ js/login.js
@@ -11,78 +11,53 @@ getDoc
 
 const email=document.getElementById("email");
 const password=document.getElementById("password");
-
 const loginBtn=document.getElementById("loginBtn");
-
 const error=document.getElementById("error");
-
 const toggle=document.getElementById("togglePassword");
-
-/* SHOW PASSWORD */
 
 toggle.onclick=()=>{
 
 if(password.type==="password"){
 
 password.type="text";
-
-toggle.classList.replace("fa-eye","fa-eye-slash");
+toggle.className="fa-solid fa-eye-slash";
 
 }else{
 
 password.type="password";
-
-toggle.classList.replace("fa-eye-slash","fa-eye");
+toggle.className="fa-solid fa-eye";
 
 }
 
 };
 
-/* LOGIN */
-
 loginBtn.onclick=async()=>{
 
 error.textContent="";
 
-const mail=email.value.trim();
-
-const pass=password.value.trim();
-
-if(mail===""||pass===""){
-
-error.textContent="Fill all fields.";
-
-return;
-
-}
-
-loginBtn.disabled=true;
-
-loginBtn.innerHTML="Please Wait...";
-
 try{
 
-const cred=await signInWithEmailAndPassword(
+const user=await signInWithEmailAndPassword(
+
 auth,
-mail,
-pass
+
+email.value,
+
+password.value
+
 );
 
-const uid=cred.user.uid;
+const uid=user.user.uid;
 
-const snap=await getDoc(
+const admin=await getDoc(
+
 doc(db,"admins",uid)
+
 );
 
-if(!snap.exists()){
-
-await auth.signOut();
+if(!admin.exists()){
 
 error.textContent="Access Denied";
-
-loginBtn.disabled=false;
-
-loginBtn.innerHTML="Login";
 
 return;
 
@@ -93,10 +68,6 @@ window.location.href="dashboard.html";
 }catch(e){
 
 error.textContent="Invalid Email or Password";
-
-loginBtn.disabled=false;
-
-loginBtn.innerHTML="Login";
 
 }
 
