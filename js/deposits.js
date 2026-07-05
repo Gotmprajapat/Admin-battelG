d9076691e34d925249e59df669acb6cb9bd71efb
Gautam <gotmprajapat00@gmail.com>
2026-07-05 11:15:59 +0530

    Update deposits.js

--- js/deposits.js
+++ js/deposits.js
@@ -178,3 +178,88 @@ alert("Reject Failed");
 }
 
 };
+
+const searchDeposit=document.getElementById("searchDeposit");
+const statusFilter=document.getElementById("statusFilter");
+
+let deposits=[];
+
+function renderDeposits(data){
+
+depositTable.innerHTML="";
+
+if(data.length===0){
+
+depositTable.innerHTML=`
+<tr>
+<td colspan="7">No Deposit Requests</td>
+</tr>
+`;
+
+return;
+
+}
+
+data.forEach(item=>{
+
+let buttons="";
+
+if(item.status==="pending"||!item.status){
+
+buttons=`
+
+<button class="actionBtn approveBtn"
+onclick="approveDeposit('${item.id}','${item.uid}',${item.amount})">
+
+Approve
+
+</button>
+
+<button class="actionBtn rejectBtn"
+onclick="rejectDeposit('${item.id}')">
+
+Reject
+
+</button>
+
+`;
+
+}else{
+
+buttons=`<b>${item.status.toUpperCase()}</b>`;
+
+}
+
+depositTable.innerHTML+=`
+<tr>
+
+<td>${item.name||"-"}</td>
+
+<td>${item.email||"-"}</td>
+
+<td>₹${item.amount}</td>
+
+<td>${item.utr}</td>
+
+<td>
+
+<button
+class="actionBtn viewBtn"
+onclick="viewImage('${item.screenshot}')">
+
+View
+
+</button>
+
+</td>
+
+<td>${item.status||"pending"}</td>
+
+<td>${buttons}</td>
+
+</tr>
+`;
+
+});
+
+}
